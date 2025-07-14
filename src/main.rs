4219ee15d569f5be67a893720018d91d2a160abe
LordAfterEight <elias@stettmayer.de>
2025-07-14 23:14:44 +0200

    Minor change

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ async fn main() {
     let player = motor::PlayerEntity::new("Player", (10.0,10.0));
     let module1 = motor::Module::new("Position");
     let module2 = motor::Module::new("Controls");
-    let entity = motor::new_entity!("Entity");
+    let module3 = motor::Module::new("Sprite");
+    let module4 = motor::Module::new("Audio");
+    let entity = motor::new_entity!("Entity", module1, module2, module3, module4);
 
     loop {
         clear_background(macroquad::color::PURPLE);
