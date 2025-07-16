1e287b693905da661c88541e8cb0af39665ebcc1
LordAfterEight <elias@stettmayer.de>
2025-07-16 22:40:53 +0200

    Got basic module interaction working

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub use log::debug;
 pub use macroquad::color;
 pub use macroquad::input;
 pub use macroquad::prelude;
 pub use macroquad;
 
-/// This macro creates a new Entity with a custom (zero or more) amount of Modules
+/// Creates a new Entity with a custom (zero or more) amount of Modules
 #[macro_export]
 macro_rules! new_entity {
     ( $name:tt ) => {
@@ -13,6 +16,8 @@ macro_rules! new_entity {
             $crate::debug!("Created new Entity instance with name '{}'", $name);
             let new_entity = $crate::Entity {
                 name: $name.to_string(),
+                val1: Default::default(),
+                val2: Default::default(),
                 id: $crate::prelude::rand::rand(),
                 modules: None
             };
@@ -25,6 +30,8 @@ macro_rules! new_entity {
             $crate::debug!("Created new Entity instance with name '{}' and module(s):", $name);
             let mut new_entity = $crate::Entity {
                 name: $name.to_string(),
+                val1: Default::default(),
+                val2: Default::default(),
                 id: $crate::prelude::rand::rand(),
                 modules: Some(Vec::new())
             };
@@ -40,14 +47,14 @@ macro_rules! new_entity {
 
 
 /// Provides 2D coordinates. Use this in ```Module::Position()```
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct Vector2D {
     pub x: f32,
     pub y: f32
 }
 
 /// Provides an image texture loaded from a file. Use this in ```Module::Texture()```
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Texture {
     pub texture: macroquad::prelude::Texture2D
 }
@@ -69,11 +76,45 @@ impl Default for Texture {
 }
 
 /// Provides keyboard and mouse input. Use this in ```Module::Controls()```
-#[derive(Debug)]
+///
+/// # Example
+/// ```
+/// use motor::Controls;
+///
+/// let controls = Module::Controls(Input::default());
+/// ```
+#[derive(Debug, Default, Clone)]
 pub struct Input {
+    pub x_value: f32,
+    pub y_value : f32
 }
 
-#[derive(Debug)]
+impl Input {
+    pub fn update(&mut self) {
+
+        self.x_value = 0.0;
+        self.y_value = 0.0;
+
+        if macroquad::input::is_key_down(macroquad::input::KeyCode::W) {
+            println!("W pressed");
+            self.y_value =  1.0;
+        }
+        if macroquad::input::is_key_down(macroquad::input::KeyCode::A) {
+            println!("A pressed");
+            self.x_value = -1.0;
+        }
+        if macroquad::input::is_key_down(macroquad::input::KeyCode::S) {
+            println!("S pressed");
+            self.y_value = -1.0;
+        }
+        if macroquad::input::is_key_down(macroquad::input::KeyCode::D) {
+            println!("D pressed");
+            self.x_value =  1.0;
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum Module {
     /// A position module containing a ```Vector2D```. This provides coordinates to an ```Entity```
     Position(Vector2D),
@@ -83,10 +124,12 @@ pub enum Module {
 }
 
 
-/// An Entity class that can have several modules (Just an idea I'm testing rn)
+/// An Entity class that can have several modules
 pub struct Entity {
     pub name: String,
     pub id: u32,
+    pub val1: f32,
+    pub val2: f32,
     pub modules: Option<Vec<Module>>,
 }
 
@@ -115,14 +158,19 @@ impl Entity {
         if let Some(modules) = &mut self.modules {
             for module in modules {
                 match module {
-                    Module::Position(vector2d) => {
-                    },
                     Module::Sprite(sprite) => {
-                        let (mut pos_x, mut pos_y) = (0.0,0.0);
-                        prelude::draw_texture(&sprite.texture, pos_x, pos_y, color::WHITE);
-                    },
-                    Module::Controls(controls) => {
+                        macroquad::prelude::draw_texture(&sprite.texture, self.val1, self.val2, macroquad::color::WHITE);
                     }
+                    Module::Controls(controls) => {
+                        controls.update();
+                        self.val1 += controls.x_value;
+                        self.val2 -= controls.y_value;
+                    }
+                    Module::Position(vector2d) => {
+                        vector2d.x += self.val1;
+                        vector2d.y += self.val2;
+                    },
+                    _ => {}
                 }
             }
         }
